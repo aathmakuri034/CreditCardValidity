bed468de2b512cf312991792df81f6e2d23594cc
Abi Athmakuri <aakv7@umsystem.edu>
2022-09-05 15:04:13 -0500

    Restarting Algorithm code

--- main.cpp
+++ main.cpp
@@ -4,43 +4,9 @@ using namespace std;
 
 bool validateCard(int cardnum, int choice){
 
-    int first = cardnum;
+    
 
-    while(first>=10){
-        first/=10;
-    }
-
-    //checks if the starting number of the card number is correct with the card provider inputed
-    if((choice == 1 && first != 4) || (choice == 2 && first != 5) || (choice == 3 && first != 3) || (choice ==4 && first != 6)){
-        return false;
-    }
-
-    int ccnum[16], num;
-
-    //converts the card number from int to array
-    for(int i=16; i>0; i--){
-        num = cardnum%10;
-        cardnum/=10;
-        ccnum[i] = num;
-    }
-
-    for(int i=0; i<16; i++){
-        cout << ccnum[i];
-    }
-
-    // int temp = cardnum, digit, total;
-    // int counter = 1;
-    // while(temp>10){
-    //     if(counter %2 == 0){
-    //         digit = temp%10;
-    //         digit*=2;
-    //         if(digit>10){
-    //             total = (digit/10) + (digit%10);
-    //         }   
-    //     }
-    // }
-
-    return false;
+    return true;
 
 }
 
