8cd5678ccf027ea2a6fd3f1bd0679629ce46d3cd
Abi Athmakuri <aakv7@umsystem.edu>
2022-09-06 10:13:33 -0500

    Card number to vector of digits finished

--- main.cpp
+++ main.cpp
@@ -1,32 +1,54 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-bool validateCard(int cardnum, int choice){
+void printArr(int arr[], int size);
 
-    
+void validateCard(long long cardnum){
 
-    return true;
+    vector<int> digits;
+    long long temp = cardnum;
+
+    while(temp){
+        digits.push_back(temp%10);
+        temp/=10;
+    }
+
+    reverse(digits.begin(), digits.end());
+
+    for(int i=0; i<digits.size(); i++){
+        cout << i << ". "<< digits.at(i) << endl;;
+    }
 
 }
 
-int main(){
-    int cardnumber, choice;
+int menu(){
 
-    cout << "Enter card number: ";
-    cin >> cardnumber; // user's credit card number is entered
+    int cardchoice;
 
     cout << "\nSelect Card Company: " << endl;
     cout << "1. Visa" << endl;
     cout << "2. Mastercard" << endl;
-    cout << "3. American Express" << endl;
-    cout << "4. Discover" << endl;
-    cin >> choice;
+    cout << "3. Discover" << endl;
+    cin >> cardchoice;
+
+    return cardchoice;
+}
+
+int main(){
+    long long cardnumber;
+
+    int choice = menu();
+
+    cout << "Enter card number: ";
+    cin >> cardnumber; // user's credit card number is entered
 
     cout << endl;
 
-    bool isValid;
-    isValid = validateCard(cardnumber, choice);
+    validateCard(cardnumber);
+
+    // Example Credit card number -> 4417123456789113
 
     // if(isValid){
     //     cout << "This is a valid card. " << endl;
